f881938408dee827be85e1f6b66f65497504ee6e
Fernanda de Oliveira Carvalho <nandaoc@pop-os.localdomain>
2021-08-30 08:25:18 -0300

    preparando alteração do tema

--- app/src/main/java/br/ufmg/coltec/trabalhofinal/views/MainActivity.java
+++ app/src/main/java/br/ufmg/coltec/trabalhofinal/views/MainActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Button;
 
 import br.ufmg.coltec.trabalhofinal.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int CURRENT_THEME = R.style.Theme_TrabalhoFinal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +30,18 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(MainActivity.this, ContactListActivity.class);
             startActivity(intent);
         });
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 }
